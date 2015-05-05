cf3222b54b0b9a93c93e5700cfcaadcfc2d342cf
DayoOliyide <dayo.oliyide@gmail.com>
2015-05-05 20:39:57 +0100

    Don't pull in logback and use in lein-bin for now.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.taoensso/timbre "3.3.1"]
-                 [twitter-streaming-client/twitter-streaming-client "0.3.2"]]
+                 [twitter-streaming-client/twitter-streaming-client "0.3.2" :exclusions [[ch.qos.logback/logback-classic]]]]
 
   :main hydrant.bin
 
