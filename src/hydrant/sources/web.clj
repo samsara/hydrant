ca6decf35a720858b781269f3098700ca5923144
DayoOliyide <dayo.oliyide@gmail.com>
2015-05-08 21:03:36 +0100

    WebSource : Web pages have their html tags removed, by default

--- src/hydrant/sources/web.clj
+++ src/hydrant/sources/web.clj
@@ -1,5 +1,6 @@
 (ns hydrant.sources.web
   (:require [itsy.core :as spider]
+            [itsy.extract :refer [html->str]]
             [hydrant.service :refer [Service]]
             [hydrant.core :refer [add-data-source events-to-flows]]
             [taoensso.timbre :as log]))
@@ -7,7 +8,7 @@
 
 (defn generate-event [root-url url body]
   {:timestamp (System/currentTimeMillis)
-   :sourceId root-url
+   :sourceId url
    :rootUrl root-url
    :eventName "web-crawl"
    :url url
@@ -19,9 +20,13 @@
   (let [event (generate-event root-url url body)]
     (events-to-flows (vector event))))
 
-(defn create-crawler [root-url url-limit]
+(defn create-crawler [root-url url-limit remove-tags]
   (spider/crawl {:url root-url
-                 :handler (fn [{:keys [url body]}] (crawler-fn root-url url body))
+                 :handler (fn [{:keys [url body]}]
+                            (if remove-tags
+                              (let [body (html->str body)]
+                                (crawler-fn root-url url body))
+                              (crawler-fn root-url url body)))
                  :workers 3
                  :url-limit url-limit
                  :url-extractor spider/extract-all
@@ -32,6 +37,7 @@
 
 (defrecord WebSource [^String root-url
                       ^Integer  url-limit
+                      remove-html-tags
                       crawler
                       running]
 
@@ -40,20 +46,20 @@
     (locking this
       (when-not @running
         (reset! running true)
-        (reset! crawler (create-crawler root-url url-limit))
-        (log/info "Started WebCrawlerSource"))))
+        (reset! crawler (create-crawler root-url url-limit remove-html-tags))
+        (log/info "Started WebSource, traversing " root-url))))
 
   (stop [this]
     (locking this
       (when @running
         (spider/stop-workers @crawler) ;;blocking call
         (reset! running false)
-        (log/info "Stopped WebCrawlerSource")))))
+        (log/info "Stopped WebSource, traversing " root-url)))))
 
 
 (defn web-source
-  [{:keys [ root-url page-crawl-limit]}]
-  (let [source (WebSource. root-url page-crawl-limit (atom nil) (atom nil))]
+  [{:keys [ root-url page-crawl-limit remove-html-tags] :or {remove-html-tags true}}]
+  (let [source (WebSource. root-url page-crawl-limit remove-html-tags (atom nil) (atom nil))]
     (add-data-source :web-source source)))
 
 (comment
@@ -63,7 +69,7 @@
       (println print-friendly)))
 
   (with-redefs [data-to-flows dtf]
-    (let [service (WebSource. "http://www.google.com" 50 (atom nil) (atom nil))]
+    (let [service (WebSource. "http://www.google.com" 50 true (atom nil) (atom nil))]
       (.start service)
       (Thread/sleep 30000) ;stay within this context (so that dtf function is used)
       (.stop service)))
