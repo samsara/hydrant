a35b4503c9abe9e277f4b0056a6fb76e1c3683fa
DayoOliyide <dayo.oliyide@gmail.com>
2015-05-11 22:16:29 +0100

    Added ability to filter flows on eventSource

--- src/hydrant/utils.clj
+++ src/hydrant/utils.clj
@@ -1,6 +1,35 @@
 (ns hydrant.utils
   (:require [taoensso.timbre :as log]))
 
+(defn match-any
+  ([poss-vals]
+   (fn [target-val]
+     (match-any poss-vals target-val)))
+
+  ([poss-vals target-val]
+   (match-any #(.equals %1 %2) poss-vals target-val))
+
+  ([f poss-vals target-val]
+   (some (partial f target-val) poss-vals)))
+
+
+(defmacro call-flows [events  flows]
+  `(do
+     ~@(map #(list % events) flows))
+;;  `(do
+;;     (doseq [flow# ~flows]
+;;       (try
+;;         (flow# ~events)
+;;         (catch Throwable t#
+;;           (log/warn t#))))
+;;  )
+)
+
+(defmacro filter-source [pred & body]
+  `(fn [events#]
+     (when (~pred (-> events# first :eventSource))
+      (call-flows events# ~body))))
+
 
 (defn sensibly-print-events [events]
   (let [one (first events)]
